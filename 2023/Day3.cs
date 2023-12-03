558aecb3efdb728e91b689b51b1e5a00042e6f0c
Samuel Couto <samuel.couto92@gmail.com>
2023-12-03 22:25:21 +0100

    Day3 part 2

--- 2023/Day3.cs
+++ 2023/Day3.cs
@@ -1,4 +1,4 @@
-﻿var input = File.ReadAllLines("input-official.txt").ToList();
+﻿var input = File.ReadAllLines("input.txt").ToList();
 
 // PART 1
 var result = 0;
@@ -39,20 +39,106 @@ for (int i = 0; i < input.Count; i++)
             }
         }
 
+        if (!char.IsNumber(c) || j == line.Length - 1)
+        {
+            if (isAdjacent && !string.IsNullOrEmpty(num))
+                result += int.Parse(num);
+            isAdjacent = false;
+            num = string.Empty;
+            continue;
+        }
+    }
+}
+
+Console.WriteLine($"Result Part 1: {result}");
+
+// PART 2
+var result2 = 0;
+var dic = new Dictionary<(int, int), List<int>>();
+for (int i = 0; i < input.Count; i++)
+{
+    var line = input[i];
+    var num = string.Empty;
+    var isAdjacent = false;
+    (int, int) coord = (-1, -1);
+    for (int j = 0; j < line.Length; j++)
+    {
+        var c = line[j];
+
+        if (char.IsNumber(c))
+        {
+            num += c.ToString();
+
+            // above line
+            if (i > 0)
+            {
+                if (input[i - 1][j] == '*')
+                {
+                    isAdjacent = true;
+                    coord = (i - 1, j);
+                }
+                else if (j > 0 && input[i - 1][j - 1] == '*')
+                {
+                    isAdjacent = true;
+                    coord = (i - 1, j - 1);
+                }
+                else if (j + 1 < line.Length && input[i - 1][j + 1] == '*')
+                {
+                    isAdjacent = true;
+                    coord = (i - 1, j + 1);
+                }
+            }
+
+            if (j > 0 && line[j - 1] == '*')
+            {
+                isAdjacent = true;
+                coord = (i, j - 1);
+            }
+            else if (j + 1 < line.Length && line[j + 1] == '*')
+            {
+                isAdjacent = true;
+                coord = (i, j + 1);
+            }
+
+            if (i + 1 < input.Count)
+            {
+                if (input[i + 1][j] == '*')
+                {
+                    isAdjacent = true;
+                    coord = (i + 1, j);
+                }
+                else if (j > 0 && input[i + 1][j - 1] == '*')
+                {
+                    isAdjacent = true;
+                    coord = (i + 1, j - 1);
+                }
+                else if (j + 1 < line.Length && input[i + 1][j + 1] == '*')
+                {
+                    isAdjacent = true;
+                    coord = (i + 1, j + 1);
+                }
+            }
+        }
 
         if (!char.IsNumber(c) || j == line.Length - 1)
         {
             if (isAdjacent && !string.IsNullOrEmpty(num))
             {
-                Console.WriteLine($"Number: {num}");
-                result += int.Parse(num);
+                if (dic.TryGetValue(coord, out var value))
+                    value.Add(int.Parse(num));
+                else
+                    dic[coord] = [int.Parse(num)];
             }
             isAdjacent = false;
             num = string.Empty;
+            coord = (-1, -1);
             continue;
         }
 
     }
 }
 
-Console.WriteLine($"Result Part 1: {result}");
+foreach(var data in dic.Where(d => d.Value.Count == 2))
+    result2 += data.Value[0] * data.Value[1];
+
+Console.WriteLine($"Result Part 2: {result2}");
